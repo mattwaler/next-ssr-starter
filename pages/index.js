82d13f15698585199d5476eb2eed0170aafef749
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2021-11-04 10:12:52 -0400

    Docker is amazing

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import User from 'models/User'
-import withSession from 'helpers/session'
 import connect from 'helpers/db'
 import Page from 'components/Page'
+import User from 'models/User'
+import withSession from 'helpers/session'
 
 export default function Home(props) {
   return (
     <Page context={props.user}>
       <div className="container py-8">
-        <h1 className="font-bold text-3xl">Home</h1>
+        <h1 className="font-bold text-3xl">Home!!!</h1>
         Hello {props.user !== null ? 'logged in user' : 'guest'}!
       </div>
     </Page>
